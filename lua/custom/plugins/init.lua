ae84a382feea223691cfecb3f64036b5c7bf30ef
Shivansh Mishra <shivanshm95@gmail.com>
2024-09-08 22:18:57 +0530

    customize settings, add custom config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- enter insert mode by default on terminals
+vim.api.nvim_create_autocmd({ 'TermOpen', 'BufEnter' }, {
+  pattern = { 'term://*' },
+  command = 'startinsert',
+})
+
+-- alias for split and open terminal
+vim.api.nvim_create_user_command('Vt', function()
+  vim.cmd '90vsplit | terminal'
+end, { nargs = 0, bang = true })
 return {}
